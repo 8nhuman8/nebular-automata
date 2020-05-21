4eff48647f1ce7fd5614837f47dd71de6bb04391
Artyom <artem.bezmenov02@gmail.com>
2020-05-21 18:57:41 +0300

    Created constants.py

---
+++ scripts/constants.py
@@ -0,0 +1,28 @@
+DESCRIPTION = ('Creates a beautiful nebula. Percentages show the duration '
+               'of further program execution in ideal conditions! In fact, '
+               'probability can take its toll.')
+
+HELP_REPRODUCE_CHANCE = 'The chance the square can produce other squares.'
+HELP_MAX_COUNT = ('The maximum number of squares in the image. By '
+                  'default, this is half of all pixels in the future image.')
+HELP_COLOR_ACCENT1 = ('The first or (primary if multicolor mode is not enabled) '
+                      'color of squares. Color components must be specified '
+                      'between 0 and 255. The The default color is aqua')
+HELP_COLOR_BACKGROUND = ('The background color. Color components must be '
+                         'specified between 0 and 255. The default color is white.')
+
+HELP_MULTICOLOR = 'Enables multicolor mode.'
+HELP_COLOR_ACCENT2 = ('The second color of squares if multicolor mode is '
+                      'enabled. Color components must be specified between 0 '
+                      'and 255. The The default color is (r, g, b): (255, 29, 119, 255).')
+
+HELP_FIND_PERCENT = ('The program will work until a nebula '
+                     'is filled with a certain percentage.')
+HELP_FADE_IN = ('The original color is white. The color of each '
+                'new generation will fade into the specified color.')
+
+HELP_SAVE = 'The generated image will be saved in the root if no path is specified.'
+HELP_PATH = ("The path by which the generated image will be saved. Write the path "
+             "without quotes, separating the directories with the usual single slash.")
+
+COLORS_METAVAR = ('R', 'G', 'B', 'A')
--- scripts/main.py
+++ scripts/main.py
@@ -6,35 +6,11 @@ from time import sleep
 
 from nebula import Nebula
 from utils import Vector, Color, generate_filename, get_runtime, get_gradient
+import constants as c
 
 
 if __name__ == '__main__':
-    description = ('Creates a beautiful nebula. Percentages show the duration '
-                   'of further program execution in ideal conditions! In fact, '
-                   'probability can take its toll.')
-    help_reproduce_chance = 'The chance the square can produce other squares.'
-    help_max_count = ('The maximum number of squares in the image. By '
-                      'default, this is half of all pixels in the future image.')
-    help_color_accent1 = ('The first or (primary if multicolor mode is not enabled) '
-                          'color of squares. Color components must be specified '
-                          'between 0 and 255. The The default color is aqua')
-    help_color_background = ('The background color. Color components must be '
-                             'specified between 0 and 255. The default color is white.')
-    help_multicolor = 'Enables multicolor mode.'
-    help_color_accent2 = ('The second color of squares if multicolor mode is '
-                          'enabled. Color components must be specified between 0 '
-                          'and 255. The The default color is (r, g, b): (255, 29, 119, 255).')
-    help_find_percent = ('The program will work until a nebula '
-                         'is filled with a certain percentage.')
-    help_fade_in = ('The original color is white. The color of each '
-                    'new generation will fade into the specified color.')
-    help_save = 'The generated image will be saved in the root if no path is specified.'
-    help_path = ("The path by which the generated image will be saved. Write the path "
-                 "without quotes, separating the directories with the usual single slash.")
-    colors_metavar = ('R', 'G', 'B', 'A')
-
-
-    parser = ArgumentParser(description=description)
+    parser = ArgumentParser(description=c.DESCRIPTION)
 
     group_required = parser.add_argument_group('Required options')
     group_required.add_argument('width', type=int, help='The width of the image.')
@@ -42,35 +18,34 @@ if __name__ == '__main__':
 
     group_basic = parser.add_argument_group('Basic options')
     group_basic.add_argument('-rc', '--reproduce-chance', metavar='FLOAT',
-                             type=float, default=0.5, help=help_reproduce_chance)
+                             type=float, default=0.5, help=c.HELP_REPRODUCE_CHANCE)
     group_basic.add_argument('-mc', '--max-count', metavar='INT',
-                             type=int, help=help_max_count)
-    group_basic.add_argument('-ca1', '--color-accent1', metavar=colors_metavar,
+                             type=int, help=c.HELP_MAX_COUNT)
+    group_basic.add_argument('-ca1', '--color-accent1', metavar=c.COLORS_METAVAR,
                              nargs=4, type=int, default=(0, 255, 255, 255),
-                             help=help_color_accent1)
-    group_basic.add_argument('-cb', '--color-background', metavar=colors_metavar,
+                             help=c.HELP_COLOR_ACCENT1)
+    group_basic.add_argument('-cb', '--color-background', metavar=c.COLORS_METAVAR,
                              nargs=4, type=int, default=(255, 255, 255, 255),
-                             help=help_color_background)
+                             help=c.HELP_COLOR_BACKGROUND)
 
     group_multicolor = parser.add_argument_group('Multicoloring options')
     group_multicolor.add_argument('-m', '--multicolor', action='store_true',
-                                  help='Enables multicolor mode.')
-    group_multicolor.add_argument('-ca2', '--color-accent2', metavar=colors_metavar,
+                                  help=c.HELP_MULTICOLOR)
+    group_multicolor.add_argument('-ca2', '--color-accent2', metavar=c.COLORS_METAVAR,
                                   nargs=4, type=int, default=(255, 29, 119, 255),
-                                  help=help_color_accent2)
+                                  help=c.HELP_COLOR_ACCENT2)
 
     group_additional = parser.add_argument_group('Additional options')
     group_additional.add_argument('-fp', '--find-percent', metavar='FLOAT',
-                                  type=float, help=help_find_percent)
+                                  type=float, help=c.HELP_FIND_PERCENT)
     group_additional.add_argument('-fi', '--fade-in', action='store_true',
-                                  help=help_fade_in)
+                                  help=c.HELP_FADE_IN)
 
     group_saving = parser.add_argument_group('Saving options')
-    group_saving.add_argument('-s', '--save', action='store_true', help=help_save)
-    group_saving.add_argument('-p', '--path', metavar='PATH', type=str, help=help_path)
+    group_saving.add_argument('-s', '--save', action='store_true', help=c.HELP_SAVE)
+    group_saving.add_argument('-p', '--path', metavar='PATH', type=str, help=c.HELP_PATH)
 
     args = parser.parse_args()
-
 
     radius = Vector(args.width, args.height)
     max_count = args.max_count
