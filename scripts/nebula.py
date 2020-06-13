feb8d6a82f7438b05f62d984f1c3fe74d1400396
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-13 16:39:39 +0300

    Make 'nebula' class methods invisible

--- scripts/nebula.py
+++ scripts/nebula.py
@@ -20,21 +20,21 @@ class Nebula:
         self.current_generation = 1
 
         self.squares = [[None for y in range(self.size.y + 1)] for x in range(self.size.x + 1)]
-        self.not_reproduced_squares = deque()
+        self._not_reproduced_squares = deque()
 
 
-    def _able_to_reproduce(self) -> bool:
+    def __able_to_reproduce(self) -> bool:
         return random() < self.reproduce_chance
 
 
-    def _output_debug_info(self) -> None:
+    def __output_debug_info(self) -> None:
         print(self.current_generation, end='\t')
         print(f'[{datetime.now().isoformat()}]', end='\t')
         print('{:.5f}'.format(self.count / self.max_count * 100), '%', end='\t')
         print(f'({self.count} / {self.max_count})')
 
 
-    def _square_reproduce(self, square: Square, generation: int) -> list:
+    def __square_reproduce(self, square: Square, generation: int) -> list:
         self.count += 1
 
         x = square.x
@@ -65,7 +65,7 @@ class Nebula:
                 and nc.x >= 0
                 and nc.y <= self.size.y
                 and nc.y >= 0
-                and self._able_to_reproduce()
+                and self.__able_to_reproduce()
             ):
                 if self.squares[nc.x][nc.y] is None:
                     self.squares[nc.x][nc.y] = Square(nc.x, nc.y, generation)
@@ -74,49 +74,49 @@ class Nebula:
         return neighboring_squares
 
 
-    def _get_next_generation(self, current_generation_squares: list) -> list:
+    def __get_next_generation(self, current_generation_squares: list) -> list:
         self.current_generation += 1
 
         next_generation_squares = []
         for square in current_generation_squares:
-            next_generation_squares.extend(self._square_reproduce(square, self.current_generation))
+            next_generation_squares.extend(self.__square_reproduce(square, self.current_generation))
         return next_generation_squares
 
 
-    def _destroy(self) -> None:
+    def __destroy(self) -> None:
         self.squares = [[None for y in range(self.size.y + 1)] for x in range(self.size.x + 1)]
         self.count = 1
         self.current_generation = 1
-        self.not_reproduced_squares = deque()
+        self._not_reproduced_squares = deque()
 
         starting_square = Square(self.starting_point.x, self.starting_point.y, self.current_generation)
         self.squares[starting_square.x][starting_square.y] = starting_square
-        self.not_reproduced_squares.append([starting_square])
+        self._not_reproduced_squares.append([starting_square])
 
 
     def develop(self, min_percent: float = None, max_percent: float = None) -> None:
         starting_square = Square(self.starting_point.x, self.starting_point.y, self.current_generation)
         self.squares[starting_square.x][starting_square.y] = starting_square
-        self.not_reproduced_squares.append([starting_square])
+        self._not_reproduced_squares.append([starting_square])
 
         if min_percent:
             while True:
-                while self.not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
-                    self._output_debug_info()
-                    current_generation_squares = self.not_reproduced_squares.popleft()
-                    self.not_reproduced_squares.append(self._get_next_generation(current_generation_squares))
+                while self._not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
+                    self.__output_debug_info()
+                    current_generation_squares = self._not_reproduced_squares.popleft()
+                    self._not_reproduced_squares.append(self.__get_next_generation(current_generation_squares))
 
                 print()
                 if self.count / self.max_count >= min_percent:
                     break
                 else:
-                    self._destroy()
+                    self.__destroy()
         elif max_percent:
             while True:
-                while self.not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
-                    self._output_debug_info()
-                    current_generation_squares = self.not_reproduced_squares.popleft()
-                    self.not_reproduced_squares.append(self._get_next_generation(current_generation_squares))
+                while self._not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
+                    self.__output_debug_info()
+                    current_generation_squares = self._not_reproduced_squares.popleft()
+                    self._not_reproduced_squares.append(self.__get_next_generation(current_generation_squares))
 
                     if self.count / self.max_count >= max_percent:
                         break
@@ -125,10 +125,10 @@ class Nebula:
                 if self.count / self.max_count >= max_percent:
                     break
                 else:
-                    self._destroy()
+                    self.__destroy()
         else:
-            while self.not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
-                self._output_debug_info()
-                current_generation_squares = self.not_reproduced_squares.popleft()
-                self.not_reproduced_squares.append(self._get_next_generation(current_generation_squares))
+            while self._not_reproduced_squares != deque([[]]) and self.count <= self.max_count:
+                self.__output_debug_info()
+                current_generation_squares = self._not_reproduced_squares.popleft()
+                self._not_reproduced_squares.append(self.__get_next_generation(current_generation_squares))
             print()
